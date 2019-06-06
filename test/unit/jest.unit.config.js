2a34e6b03211ac9604a8f0294216d214df326995
Tom Carter <heymrcarter14@gmail.com>
2019-06-05 21:20:20 -0400

    updates and security fixes

--- test/unit/jest.unit.config.js
+++ test/unit/jest.unit.config.js
@@ -3,8 +3,8 @@ const path = require('path')
 module.exports = {
   rootDir: path.resolve(path.join(__dirname, '../..')),
   moduleFileExtensions: ['js', 'json'],
-  setupTestFrameworkScriptFile: '<rootDir>/test/unit/td.setup',
   setupFiles: ['<rootDir>/test/unit/test.env.setup'],
+  setupFilesAfterEnv: ['<rootDir>/test/unit/td.setup'],
   coverageDirectory: '<rootDir>/.test/unit/coverage',
   collectCoverageFrom: ['<rootDir>/src/**/*.js'],
   testPathIgnorePatterns: ['<rootDir>/test/integration'],
